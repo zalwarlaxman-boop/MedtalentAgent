1ec3c9c4e4375e9eb22fb816d23434084332f941
zalwarlaxman-boop <zalwarlaxman-boop@users.noreply.github.com>
2026-04-27 11:54:38 +0000

    Revert commits 7641774..98860d1
    This reverts 15 commit(s) from 7641774 to 98860d1.
    
    Reverted commits:
    98860d1 feat: 生成项目Code Wiki文档
    b88927a feat: 生成项目Code Wiki文档
    1ef05ef feat: 生成项目Code Wiki文档
    9a5ccca Revert commits 04cc835..a0a761b
    a0a761b feat: 生成项目Code Wiki文档
    3029ed4 Revert commits 04cc835..30d7fe9
    30d7fe9 feat: 生成项目Code Wiki文档
    b36e710 Revert commits af8c3fa..c0c178c
    c0c178c feat: 生成项目Code Wiki文档
    af8c3fa feat: 生成项目Code Wiki文档
    d6a81cc feat: 生成项目Code Wiki文档
    04cc835 feat: 生成项目Code Wiki文档
    1e91967 Revert commits 7641774..7ca7be6
    7ca7be6 feat: 生成项目Code Wiki文档
    7641774 feat: 生成项目Code Wiki文档
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- zheli-wutong/src/main.jsx
+++ zheli-wutong/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MainLayout from './portal/MainLayout'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainLayout />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
